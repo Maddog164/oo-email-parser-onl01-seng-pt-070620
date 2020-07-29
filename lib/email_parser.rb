7109a0c665945fd5e0637401a2c3e4354d95c6d7
Denise Henderson <denise@henderson3.us>
2020-07-29 23:36:19 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,7 +4,7 @@
 # or whitespace (' ').
 class EmailAddressParser
 
-  attr_accessor :email_addresses
+  attr_accessor :name :email_addresses
 
   def parse(email_addresses)
     @email_array << email_addresses.split(/[\s,]/)
